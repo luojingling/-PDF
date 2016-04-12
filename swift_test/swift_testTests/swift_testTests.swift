2b1abd764d20386730cfc60b8fe432842ad16c3b
ljl <1304622859@qq.com>
2016-04-12 10:58:36 +0800

    dsds

---
+++ swift_test/swift_testTests/swift_testTests.swift
@@ -0,0 +1,36 @@
+//
+//  swift_testTests.swift
+//  swift_testTests
+//
+//  Created by 罗精灵 on 15/12/8.
+//  Copyright © 2015年 luojingling. All rights reserved.
+//
+
+import XCTest
+@testable import swift_test
+
+class swift_testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
